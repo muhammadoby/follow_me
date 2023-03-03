078120eee4706115bdd783061c7f2a265d471c44
Muhammad Oby <96622188+muhammadoby@users.noreply.github.com>
2023-03-03 12:51:35 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+VANTA.BIRDS({
+
+  el: "#bird",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color1: 0x1c97be,
+  color2: 0x1560e8
+})
